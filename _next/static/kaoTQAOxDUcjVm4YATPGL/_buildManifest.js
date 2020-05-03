75198ff75d2cbe7e074e5f1c811720b8c9f652f5
hafidzamr <hafidz@qoala.id>
2020-05-03 15:39:28 +0700

    Updates

---
+++ _next/static/kaoTQAOxDUcjVm4YATPGL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F95b64a6e.a5503fa877e62c927ead.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
