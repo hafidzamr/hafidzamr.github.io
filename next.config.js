bdcb4e9be1da05250819a0017be7f48af7bfad79
hafidzamr <hafidz@qoala.id>
2020-05-03 18:43:17 +0700

    fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,33 +1,28 @@
 const withCss = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 const withPlugins = require("next-compose-plugins");
-
 const debug = process.env.NODE_ENV !== "production";
 
-module.exports = withPlugins(
+module.exports = withPlugins([
 	[
-		[
-			withCss,
-			{
-				cssModules: false,
-				cssLoaderOptions: {
-					importLoaders: 1,
-					localIdentName: "[local]___[hash:base64:5]"
-				}
+		withCss,
+		{
+			cssModules: true,
+			cssLoaderOptions: {
+				importLoaders: 1,
+				localIdentName: "[local]___[hash:base64:5]"
 			}
-		],
-		[
-			withSass,
-			{
-				cssModules: true,
-				cssLoaderOptions: {
-					importLoaders: 1,
-					localIdentName: "[local]___[hash:base64:5]"
-				}
-			}
-		]
+		}
 	],
-	{
-		assetPrefix: !debug ? "/" : ""
-	}
-);
+	[
+		withSass,
+		{
+			cssModules: true,
+			cssLoaderOptions: {
+				importLoaders: 1,
+				localIdentName: "[local]___[hash:base64:5]"
+			}
+		}
+	],
+	{ assetPrefix: !debug ? "https://hafidzamr.github.io/" : "" }
+]);
