231ac3f8c873c2b0e21294ddc1d28e7d5ad5bdd6
Hafidz <hafidzdev@gmail.com>
2021-02-07 13:29:04 +0700

    trying to fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
-const debug = process.env.NODE_ENV !== 'production';
 
-module.exports = withPlugins([
-  [withImages],
-  { assetPrefix: !debug ? 'https://hafidzamr.github.io' : '' }
-]);
+module.exports = withPlugins([[withImages]]);
