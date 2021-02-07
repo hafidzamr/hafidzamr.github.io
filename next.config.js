b13aebad4a26cc3d3a7892cad4202a9200ea8eeb
Hafidz <hafidzdev@gmail.com>
2021-02-07 13:08:10 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
+const debug = process.env.NODE_ENV !== 'production';
 
-module.exports = withImages();
+module.exports = withPlugins([
+  withImages,
+  { assetPrefix: !debug ? 'https://hafidzamr.github.io/' : '' }
+]);
