b694ff502b222c1f921eb632a683010d8bd29144
Hafidz <hafidzdev@gmail.com>
2021-02-09 23:32:06 +0700

    fix color link navbar to black

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -5,19 +5,19 @@ const Navbar = () => {
     <nav className='my-0 px-8 h-14 flex justify-between items-center'>
       <Link href='/'>
         <a
-          className='text-black hover:text-black visited:text-black'
+          className=' hover:text-black visited:text-black'
           rel='noopener noreferrer'
         >
-          <div className='font-bold text-xl'>Hafidz Amrulloh</div>
+          <div className='font-bold text-xl text-black'>Hafidz Amrulloh</div>
         </a>
       </Link>
 
       <Link href='/blog'>
         <a
-          className='text-black hover:text-black visited:text-black'
+          className=' hover:text-black visited:text-black'
           rel='noopener noreferrer'
         >
-          <div className='font-bold text-xl'>Blog</div>
+          <div className='font-bold text-xl text-black'>Blog</div>
         </a>
       </Link>
     </nav>
